d8940636cc22f4ed53d4921bb3a760e20ee744a8
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-02-14 00:38:37 +0200

    no-cors

--- pages/import-products/index.tsx
+++ pages/import-products/index.tsx
@@ -50,6 +50,7 @@ const importProducts = () => {
         // http://localhost:8080/send
         fetch('https://stock-assistant-friendsofcomme.herokuapp.com/send', {
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -101,6 +102,7 @@ const importProducts = () => {
         //http://localhost:8080/subscribe
         fetch('https://stock-assistant-friendsofcomme.herokuapp.com/subscribe', {
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json'
             },
