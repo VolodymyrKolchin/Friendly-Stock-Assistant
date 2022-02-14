5df9095213f20daa41192856886797654bd7b05d
Volodymyr Kolchin <vkolchin@codifi.me>
2022-02-14 15:44:39 +0200

    FOC-66

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -21,11 +21,12 @@ const Products = () => {
       ...(columnHash && { direction: direction.toLowerCase() }),
     });
     const itemsPerPageOptions = [10, 20, 50, 100];
-    const tableItems: TableItem[] = list.map(({ id, inventory_level: stock, name, price }) => ({
+    const tableItems: TableItem[] = list.map(({ id, inventory_level: stock, name, price, variants }) => ({
         id,
         name,
         price,
         stock,
+        variants,
     }));
 
     const dataProductVariants= [];
@@ -49,7 +50,7 @@ const Products = () => {
         setDirection(newDirection);
     };
 
-    const renderName = (id: number, name: string, variants: Array<Object>): ReactElement => (
+    const renderName = (id: number, name: string): ReactElement => (
         <Link href={`/products/${id}`}>
             <StyledLink>{name}</StyledLink>
         </Link>
