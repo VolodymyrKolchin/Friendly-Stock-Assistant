fba5f86e9508b83a01dbe5bacfe5652ed613aee9
Volodymyr Kolchin <vkolchin@codifi.me>
2022-02-14 15:44:12 +0200

    FOC-66

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -21,12 +21,11 @@ const Products = () => {
       ...(columnHash && { direction: direction.toLowerCase() }),
     });
     const itemsPerPageOptions = [10, 20, 50, 100];
-    const tableItems: TableItem[] = list.map(({ id, inventory_level: stock, name, price, variants }) => ({
+    const tableItems: TableItem[] = list.map(({ id, inventory_level: stock, name, price }) => ({
         id,
         name,
         price,
         stock,
-        variants,
     }));
 
     const dataProductVariants= [];
