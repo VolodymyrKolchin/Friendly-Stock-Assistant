3431374e0ed868855c646e2af76a5c86d6d17850
Volodymyr Kolchin <vkolchin@codifi.me>
2022-02-15 10:54:34 +0200

    FOC-66

--- public/accordion.js
+++ public/accordion.js
@@ -1,9 +1,10 @@
-const acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-for (let i = 0; i < acc.length; i++) {
+for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        const panel = this.nextElementSibling;
+        var panel = this.nextElementSibling;
         if (panel.style.maxHeight){
             panel.style.maxHeight = null;
         } else {
