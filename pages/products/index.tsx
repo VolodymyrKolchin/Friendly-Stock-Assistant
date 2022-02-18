0b305574b8d23cd209484a90ff41f8aaff8c3df8
Volodymyr Kolchin <vkolchin@codifi.me>
2022-02-18 16:21:10 +0200

    inventory_level

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -152,7 +152,7 @@ const Products = () => {
                                    SKU: {element.sku}
                                 </td>
                                 <td className="product-stock">
-                                   {element.inventory_level < element.inventory_warning_level
+                                   {element.inventory_level < element.inventory_warning_level && element.inventory_level !==0
                                         ? <span className='background_light_yellow background_stock' >Stock: {element.inventory_level}</span>
                                         : <span>{element.inventory_level==0 ? <span className='background_red background_stock'>Stock: {element.inventory_level}</span>:<span>Stock: {element.inventory_level}</span>}</span>
                                    }
