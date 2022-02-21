3a9d7089652419f44567d2c868168bcd47461f96
Volodymyr Kolchin <vkolchin@codifi.me>
2022-02-21 14:45:15 +0200

    route

--- pages/api/import-products/index.ts
+++ pages/api/import-products/index.ts
@@ -3,10 +3,30 @@ import { bigcommerceClient, getSession } from '../../../lib/auth';
 
 export default async function importProducts(req: NextApiRequest, res: NextApiResponse) {
     try {
+        const dataEmail = [];
+        fetch('https://stock-assistant-friendsofcomme.herokuapp.com/email-list', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                //storeHash: data?.storeHash,
+                storeHash: `${storeHash}`
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            data.message[0].forEach((el)=>{
+                dataEmail.push(el.email);
+            })
+            console.log('1!dataEmail', dataEmail);
+        })
+        console.log('dataEmail2', dataEmail);
+
         const { accessToken, storeHash } = await getSession(req);
         const bigcommerce = bigcommerceClient(accessToken, storeHash);
         const { data } = await bigcommerce.get('/catalog/products?include=variants');
-        res.status(201).json({data, accessToken: accessToken, storeHash: storeHash});
+        res.status(201).json({data, accessToken: accessToken, storeHash: storeHash, dataEmail:dataEmail});
     } catch (error) {
         const { message, response } = error;
         res.status(response?.status || 500).json({ message });
