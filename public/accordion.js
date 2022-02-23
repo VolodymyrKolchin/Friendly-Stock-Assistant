76e020b83037105dc8d03cbc5445e65c8fb272bb
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-02-24 01:24:09 +0200

    fix commit

--- public/accordion.js
+++ public/accordion.js
@@ -13,3 +13,18 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
+var accItem = document.getElementsByClassName("form-control-delete");
+var iItem;
+
+for (iItem = 0; iItem < accItem.length; i++) {
+    accItem[iItem].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
