75f70887a9f880f2cc6dbae6b69d623ccd82ac6a
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-03-03 01:04:11 +0200

    commit

--- pages/import-products/index.tsx
+++ pages/import-products/index.tsx
@@ -31,6 +31,7 @@ const importProducts = () => {
         clientData.push(process.env.CLIENT_ID);
         console.log("document.getElementById('month')", document.getElementById('month'));
         console.log("document.getElementById('month').getElementsByTagName('li')", document.getElementById('month').getElementsByTagName('li'));
+        console.log("document.getElementById('month').getElementsByTagName('li').length", document.getElementById('month').getElementsByTagName('li').length);
         if(document.getElementById('month').getElementsByTagName('li').length == 0) {
             console.log('month');
             document.getElementById('month').classList.add("hide");
