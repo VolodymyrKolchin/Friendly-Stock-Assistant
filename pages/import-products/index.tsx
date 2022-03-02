05537f857a1fa657bb60d628b115a69094f1c7a0
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-03-02 18:21:18 +0200

    commit

--- pages/import-products/index.tsx
+++ pages/import-products/index.tsx
@@ -96,7 +96,7 @@ const importProducts = () => {
         console.log('window.location', window.location);
         console.log('window.location.pathname', window.location.pathname);
         //router.reload(); //пропадает спимок, когда страница перезагружается
-        window.location.reload() // window.location.reload()
+        //window.location.reload() // window.location.reload()
         if(form.email === '' ) {
             setIsLoadingSubscribeShowEmail(true);
             return;
