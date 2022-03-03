d8015cb0017648aafca1b70f85bcd994e93a2518
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-03-03 14:39:39 +0200

    commit

---
+++ pages/api/subscriptions/index.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { bigcommerceClient, getSession } from '../../../lib/auth';
+
+export default async function subscriptionsList(req: NextApiRequest, res: NextApiResponse) {
+    try {
+
+        const { accessToken, storeHash } = await getSession(req);
+        const bigcommerce = bigcommerceClient(accessToken, storeHash);
+
+        const dataEmail = [];
+        fetch('https://stock-assistant-friendsofcomme.herokuapp.com/email-list', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                storeHash: `${storeHash}`
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            data.message[0].forEach((el)=>{
+                dataEmail.push(el);
+            })
+        })
+
+        const { data } = await bigcommerce.get('/catalog/products?include=variants');
+        res.status(201).json({data, accessToken: accessToken, storeHash: storeHash, dataEmail});
+    } catch (error) {
+        const { message, response } = error;
+        res.status(response?.status || 500).json({ message });
+    }
+}
