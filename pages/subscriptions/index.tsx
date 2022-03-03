5e23ceea5325604f5aeb9a15811f7a47d909bc62
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-03-03 14:39:39 +0200

    commit

--- pages/subscriptions/index.tsx
+++ pages/subscriptions/index.tsx
@@ -1,7 +1,15 @@
 import {Panel} from '@bigcommerce/big-design';
+import {useProductListAll} from '../../lib/hooks';
 
 const subscriptionsPage = () => {
+    const { error, isLoading, list = [], meta = {}, mutateList=[], data } = useProductListAll();
 
+    const clientData = [];
+
+    if(!isLoading) {
+        console.log('isLoading data', data)
+    }
+    
     return (
         <Panel header='Subscriptions page'>
         <div>
