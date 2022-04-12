97d8310325f3336638a3a7c1b0be82a94472b5d4
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-04-12 13:25:04 +0300

    commit

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -161,7 +161,7 @@ const Products = () => {
                             ${el.price}
                         </td>
                         <td className="product-cost-price">
-                            {(el.cost_price/el.price)*100}.toFixed(1)%
+                            {((el.cost_price/el.price).toFixed(1))*100}%
                         </td>
                     </tr>
                     <div className="panel">
