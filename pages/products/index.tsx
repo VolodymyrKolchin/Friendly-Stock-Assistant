b2b92094f98ecbf022fc0cc1c159d3521c0e3cf9
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-04-15 00:33:24 +0300

    commit

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -155,7 +155,7 @@ const Products = () => {
             </thead>
             <tbody>
             {tableItems.map((el)=>{
-                console.log('el.variant[0]', el.variant[0]);
+                console.log('el.variants[0]', el.variants[0].image_url);
                 let cost = ((el.cost_price/el.price)*100).toFixed(2);
                 return(
                 <>
