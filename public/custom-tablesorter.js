b0abf3fef02c7da81dece28be901afe29d6a6052
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-04-17 22:53:42 +0300

    commit

--- public/custom-tablesorter.js
+++ public/custom-tablesorter.js
@@ -1,3 +1,10 @@
 $(function() {
-    $("#myTable").tablesorter();
+    // $("#myTable").tablesorter();
+    $("#myTable").tablesorter({
+        theme : 'dropbox',
+        cssIcon: 'tablesorter-icon',
+        initialized : function(table){
+          $(table).find('thead .tablesorter-header-inner').append('<i class="tablesorter-icon"></i>');
+        }
+      });
 });
